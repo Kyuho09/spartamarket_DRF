e9c1c28769976adf2f8c10e63d2df112390a04a1
Kyuho09 <noaetoile1@gmail.com>
2024-09-08 18:19:48 +0900

    feat: 회원가입 기능 구현
    accounts/views.py의 SignupView 구현
    임시로 구현하여 print로 결과를 확인

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from .models import User
 
 # Create your views here.
 class SignupView(APIView):
-    pass
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        nickname = request.data.get("nickname")
+        birth = request.data.get("birth")
+        first_name = request.data.get("first_name")
+        last_name = request.data.get("last_name")
+        email = request.data.get("email")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            nickname=nickname,
+            birth=birth,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+        )
+        print(user)
+
+        return Response({})
