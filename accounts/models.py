8c50c1f5f39c0d17d12f1b9d77187ecaf39b0328
Kyuho09 <noaetoile1@gmail.com>
2024-09-08 18:00:07 +0900

    feat: User 모델 커스텀
    필수 입력 필드 : nickname, birth
    선택 입력 필드 : gender, introductions
    이후 migrate까지 진행

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    nickname = models.CharField(max_length=20)
+    birth = models.DateField()
+    gender = models.CharField(max_length=10, null=True, blank=True)
+    introductions = models.TextField(null=True, blank=True)
