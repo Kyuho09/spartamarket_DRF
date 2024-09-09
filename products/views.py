658676dad1f9c77f3dd75d782608dac7808a0944
Kyuho09 <noaetoile1@gmail.com>
2024-09-09 17:19:23 +0900

    feat: serializer를 통해 view 단축
    products/serializers.py 생성
    Product 모델을 상속받아 우선 모든 필드를 사용
    serializer를 사용하여 view 수정

--- products/views.py
+++ products/views.py
@@ -1,34 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 class ProductListView(APIView):
     def post(self, request):
         title = request.data.get("title")
         content = request.data.get("content")
-        
         product = Product.objects.create(title=title, content=content)
-        res_json = {
-            "id": product.id,
-            "title": product.title,
-            "content": product.content,
-            "created_at": product.created_at,
-            "updated_at": product.updated_at,
-        }
-        return Response(res_json)
-    
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
     def get(self, request):
         products = Product.objects.all()
-        res_json = []
-        for product in products:
-            res_json.append(
-                {
-                    "id": product.id,
-                    "title": product.title,
-                    "content": product.content,
-                    "created_at": product.created_at,
-                    "updated_at": product.updated_at,
-                }
-            )
-        return Response(res_json)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
