167c80a4aa24ec3fed25bd36019e6b6279cb3cc8
Kyuho09 <noaetoile1@gmail.com>
2024-09-09 18:16:47 +0900

    feat: 게시글 수정 기능 구현
    게시글 수정 기능 구현

--- products/views.py
+++ products/views.py
@@ -24,3 +24,11 @@ class ProductDetailView(APIView):
         product = get_object_or_404(Product, pk=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
