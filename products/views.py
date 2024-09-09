c51518b26130ecf13459c89669ae08c556b1748c
Kyuho09 <noaetoile1@gmail.com>
2024-09-09 17:34:16 +0900

    feat: 게시글 상세 페이지 조회 기능 구현
    ProductDetailView 구현
    get_object_or_404를 사용하여 처리

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -15,4 +16,11 @@ class ProductListView(APIView):
     def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+class ProductDetailView(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
