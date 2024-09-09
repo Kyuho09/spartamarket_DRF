af5c6bd690c67777584f5a943e0515fc341099a2
Kyuho09 <noaetoile1@gmail.com>
2024-09-09 16:54:22 +0900

    feat: ProductListView 구현
    ProductListView 구현
    models 커스텀 모델명 Product로 변경

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
 
 # Create your views here.
+class ProductListView(APIView):
+    def post(self, request):
+        title = request.data.get("title")
+        content = request.data.get("content")
+        
+        product = Product.objects.create(title=title, content=content)
+        res_json = {
+            "id": product.id,
+            "title": product.title,
+            "content": product.content,
+            "created_at": product.created_at,
+            "updated_at": product.updated_at,
+        }
+        return Response(res_json)
+
+class ProductDetailVeiw:
+    pass
